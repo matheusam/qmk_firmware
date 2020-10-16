6f6afff701f91ca48efdc842a991c78b2a1c6f8a
Matheus Acosta <matheusthebr@gmail.com>
2020-10-16 11:38:46 -0300

    Added unicode keys.

--- keyboards/kbdfans/kbd67/rev2/keymaps/matheusam/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/matheusam/keymap.c
@@ -1,20 +1,23 @@
 #include QMK_KEYBOARD_H
 
+#define BASE 0
+#define COMMAND 1
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap (Base Layer) Default Layer
      * ,----------------------------------------------------------------.
      * |Esc|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Home|
      * |----------------------------------------------------------------|
-     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |PgUp|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \| |PgUp|
      * |----------------------------------------------------------------|
-     * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgDn|
+     * |Ctrl   |  A|  S|  D|  F|  G|  H|  J|  K|  L| :;| "'|Return |PgDn|
      * |----------------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|End |
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M| <,| .>|  /|Shift | Up|End |
      * |----------------------------------------------------------------|
      * |Ctrl|Meta |Alt |        Space         |Alt| FN|Ctrl|Lef|Dow|Rig |
      * `----------------------------------------------------------------'
      */
-	[0] = LAYOUT_65_ansi(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_HOME,
+	[BASE] = LAYOUT_65_ansi(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_HOME,
 											 KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
 											 KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
 											 KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_END,
@@ -25,28 +28,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |     |   |   |   |  |   |   |   |   |   |PSCR|   |   |     |    |
    * |----------------------------------------------------------------|
-   * |      |   |  |  |  |  |   |   |   |RGB_TOG|   |   |        |    |
+   * |      |   |  |  |  |  |   |   |   |RGB_TOG| ^ | ´ |        |    |
    * |----------------------------------------------------------------|
-   * |        |   |   |   |   |   |   |   |   |   |   |     |VOLU|    |
+   * |        |   |   | ç |   |   |   |   |   |   |   |     |VOLU|    |
    * |----------------------------------------------------------------|
    * |    |    |    |                    |   |   |    |BRID|VOLD|BRIU |
    * `----------------------------------------------------------------'
    */
-	[1] = LAYOUT_65_ansi(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS,
+	[COMMAND] = LAYOUT_65_ansi(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, KC_INS,
 											 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-											 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-											 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_TRNS,
+											 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RGB_TOG, UC(0x005E), UC(0x00B4), KC_TRNS, KC_TRNS,
+											 KC_TRNS, KC_TRNS, KC_TRNS, UC(0x00E7), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLU, KC_TRNS,
 											 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BRID, KC_VOLD, KC_BRIU)
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
 #ifdef RGBLIGHT_ENABLE
     switch (get_highest_layer(state)) {
-      case 0:
-        rgblight_sethsv_range(HSV_YELLOW, 0, RGBLED_NUM);
+      case BASE:
+        rgblight_sethsv_range(HSV_GREEN, 0, RGBLED_NUM);
+        break;
+			case COMMAND:
+        rgblight_sethsv_range(HSV_RED, 0, RGBLED_NUM);
         break;
       default:
-        rgblight_sethsv_range(HSV_GREEN, 0, RGBLED_NUM);
+        rgblight_sethsv_range(HSV_YELLOW, 0, RGBLED_NUM);
         break;
     }
     rgblight_set_effect_range(0, 16);
