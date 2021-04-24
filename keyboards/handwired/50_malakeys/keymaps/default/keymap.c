a59f1ff2e2af286bd63524b9caf057feb7612aae
Harry <matheusthebr@gmail.com>
2021-04-24 18:47:43 -0300

    Fixed enter podition malakeys default keymap.

--- keyboards/handwired/50_malakeys/keymaps/default/keymap.c
+++ keyboards/handwired/50_malakeys/keymaps/default/keymap.c
@@ -16,20 +16,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *,-----------------------------------------------------------------------------------.
    *| ESC  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
    *|------+------+------+------+------+------+------+------+------+------+------+------|
-   *| TAB  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Enter|
+   *| TAB  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | ` ~  |
    *|------+------+------+------+------+-------------+------+------+------+------+------|
    *| CAPS |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | ; :  | ' "  |
    *|------+------+------+------+------+------|------+------+------+------+------+------|
-   *| Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  | , <  | . >  |  Up  | ` ~  |
+   *| Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  | , <  | . >  |  Up  | Enter|
    *|------+------+------+------+------+------+------+------+------+------+------+------|
    *| Ctrl | GUI  | Alt  |  FN  |Lower |    Space    |Raise |  / ? | Left | Down | Right|
    *`-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT(
     KC_ESC,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_BSPC,
-    KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_ENT,
+    KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_GRAVE,
     KC_CAPSLOCK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_UP,   KC_GRAVE,
+    KC_LSFT,     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_UP,   KC_ENT,
     KC_LCTL,     KC_LGUI, KC_LALT, FN,      LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLASH, KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -68,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *`-----------------------------------------------------------------------------------'
    */
   [_RAISE] = LAYOUT(
-    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,     KC_ASTR,     KC_LPRN,     KC_RPRN,     KC_DELETE,
-    _______, _______, _______, _______, _______, _______, _______, KC_KP_MINUS, KC_KP_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_BSLASH,
-    _______, _______, _______, _______, _______, _______, _______, _______,     _______,     _______,     _______,     _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,     _______,     _______,     KC_PGUP,     _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,     _______,     KC_HOME,     KC_PGDOWN,   KC_END
+    _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,     KC_LPRN,     KC_RPRN,     KC_DELETE,
+    _______, _______, _______, _______, _______, _______, _______, KC_MINUS, KC_KP_EQUAL, KC_LBRACKET, KC_RBRACKET, KC_BSLASH,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______,     _______,     _______,     _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______,     _______,     KC_PGUP,     _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  _______,     KC_HOME,     KC_PGDOWN,   KC_END
   ),
 
   /* Fn (F keys and random controls)
